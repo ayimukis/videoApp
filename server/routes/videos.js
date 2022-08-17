e3e934b4d5944ba59d8293f817b45e2025d61b4d
김찬혁 <ayimukis1@gmail.com>
2022-08-17 17:57:05 +0900

    Initial commit

---
+++ server/routes/videos.js
@@ -0,0 +1,17 @@
+import express from "express";
+import {addVideo, addView, deleteVideo, getVideo, random, sub, trend, updateVideo} from "../controllers/video.js"
+import {verifyToken} from "../verifyToken.js";
+
+const router = express.Router();
+
+//create a video
+router.post("/", verifyToken, addVideo);
+router.put("/:id", verifyToken, updateVideo);
+router.delete("/:id", verifyToken, deleteVideo);
+router.get("/find/:id", getVideo);
+router.put("/view/:id", addView);
+router.get("/trend/:id", trend);
+router.get("/random/:id", random);
+router.get("/sub/:id", verifyToken, sub);
+
+export default router;
